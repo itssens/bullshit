25d30719aef34e1b375d444c24cda8d9aaf84248
Sens <122377164+itssens@users.noreply.github.com>
2023-01-10 17:23:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import modules.sense
+import time
+import os
 
-sense = modules.sense.gamesense(6)
-val = sense.getVal()
+def clearcmd():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def rechner():
+  sense = modules.sense.gamesense(6)
+  val = sense.getVal()
 
-print(val)
+  print(val)
+  
+def main():
+  print("Module:")
+  print("[1] Automatischer Rechner")
+  print("Weiteres kommt bald...")
+  eingabe = input("Welches Modul willst du starten?")
+  if eingabe == "1":
+    rechner()
+  elif eingabe == "2":
+    print("bald....")
+    time.sleep(2.00)
+    clearcmd()
+    main()
+  else:
+    ungültig = input("Ungültige Eingabe! Drücke enter um es erneut zu probieren....")
+    clearcmd()
+    main()
+main()
